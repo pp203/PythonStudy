07bd9eec96fcf37a45daf77f8732d5d821180590
Yamada <seinay@b0be83474ff4.ant.amazon.com>
2023-08-06 15:41:17 -0400

    Self Study

---
+++ SelfStudy/Chapter02/StudyNote/list06.py
@@ -0,0 +1,16 @@
+data = [
+    [
+        ['a','b','c'],
+        ['A','B','B'],
+    ],
+    [
+        ['aa','bb','cc','dd'],
+        ['AA','BB','CC'],
+    ],
+    [
+        ['aaa','bbb','ccc','ddd','eee'],
+        ['AAA','BBB','CCC','DDD'],
+    ]
+]
+
+print(data[2][0][4])
